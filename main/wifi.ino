09f16af3d5b30ffeb1bb097bef24f3589bba559a
Unknown <x.correyeur@free.fr>
2020-04-08 00:48:20 +0200

    Wifi: enhance connexion + reconnexion process

--- main/wifi.ino
+++ main/wifi.ino
@@ -8,9 +8,6 @@
 void wifi_setup() {
 
   pinMode(wifi_LedPin, OUTPUT);
-
-  digitalWrite(wifi_LedPin, LOW);
-  delay(1000);
   digitalWrite(wifi_LedPin, HIGH);
 }
 
@@ -21,9 +18,11 @@ void wifi_manage(int checkDelay) {
 
     if ( WiFi.status() != WL_CONNECTED ) {
 
-      digitalWrite(wifi_LedPin, HIGH);
-
-      __wifi_connect();
+      if ( !__wifi_connect() ) {
+        Serial.print("WiFi connexion retry in ");
+        Serial.print(checkDelay/1000);
+        Serial.println("s....");
+      }
 
       delay(checkDelay);
     }
@@ -37,9 +36,7 @@ bool wifi_status() {
 }
 
 /* Internal method to connect to WIFI */
-void __wifi_connect() {
-
-  delay(10);
+bool __wifi_connect() {
 
   // We start by connecting to a WiFi network
   Serial.println();
@@ -47,17 +44,28 @@ void __wifi_connect() {
   Serial.println(ssid);
 
   WiFi.begin(ssid, password);
+  delay(2000);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  int counter = 0;
+  while (WiFi.status() != WL_CONNECTED && counter < 30) {
+    Serial.println("WiFi not connected... waiting...");
     digitalWrite(wifi_LedPin, HIGH);
     delay(300);
-    Serial.print(".");
     digitalWrite(wifi_LedPin, LOW);
     delay(200);
+    counter++;
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  if ( WiFi.status() == WL_CONNECTED ) {
+    digitalWrite(wifi_LedPin, LOW);
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    return true;
+  }
+
+  digitalWrite(wifi_LedPin, HIGH);
+  Serial.println("WiFi connexion failed !");
+  return false;
 }
