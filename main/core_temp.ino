8d42d90f8afb40590c1130ae3aa6bbfc81148578
Unknown <x.correyeur@free.fr>
2020-04-10 09:26:09 +0200

    core_temp: update mqtt topic

--- main/core_temp.ino
+++ main/core_temp.ino
@@ -13,12 +13,12 @@ void core_temp( void * pvParameters ){
     while(true){
     
       Serial.println(dht22_getJson("dht0"));
-      mqtt_publish("temphum/dht22", dht22_getJson("dht0"));
+      mqtt_publish("esp32swim/dht22", dht22_getJson("dht0"));
 
       delay(1000);
 
       Serial.println(ds18b20_getJson());
-      mqtt_publish("temp/ds18b20", ds18b20_getJson());
+      mqtt_publish("esp32swim/ds18b20", ds18b20_getJson());
       
       delay(10000);
     } 
