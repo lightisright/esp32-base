7a2fc5662f9daa0be0c030e419d2b8ad87eed2ff
Unknown <x.correyeur@free.fr>
2020-06-25 17:35:15 +0200

    MQTT callback management

--- main/mqtt_callback.ino
+++ main/mqtt_callback.ino
@@ -1,52 +1,60 @@
 void mqtt_subscriptions() {
-  client.subscribe("esp32swim");
-  client.subscribe("esp32swim/heater");
-  client.subscribe("esp32swim/pump");
+
+  String topic;
+
+  topic = esp32_id+"/heater";
+  client.subscribe(topic.c_str());
+  topic = esp32_id+"/pump";
+  client.subscribe(topic.c_str());
+  topic = esp32_id+"/restart";
+  client.subscribe(topic.c_str());
 }
 
 
 /* for MQTT subscription if needed */
 void mqtt_callback(char* topic, byte* message, unsigned int length) {
   
-  Serial.print("Message arrived on topic: ");
-  Serial.print(topic);
-  Serial.print(". Message: ");
   String messageTemp;
-  String notification;
-  
+  String topic_str = String(topic);
+
   for (int i = 0; i < length; i++) {
-    Serial.print((char)message[i]);
     messageTemp += (char)message[i];
   }
-  Serial.println();
 
-  // Feel free to add more if statements to control more GPIOs with MQTT
-  Serial.println(String(topic)+" : "+messageTemp);
-
+  Serial.print("Message arrived on topic: "+topic_str+". Message: "+messageTemp);
+  mqtt_pub_notify(topic_str, messageTemp);
+  
   // esp32swim/pump
-  if (String(topic) == "esp32swim/pump") {
+  if (mqtt_match_topic(topic_str, "pump")) {
     if(messageTemp == "on"){
-      Serial.println("on");
       digitalWrite(pump_RelayPin, HIGH);
     }
     else if(messageTemp == "off"){
-      Serial.println("off");
       digitalWrite(pump_RelayPin, LOW);
     }
-    notification = "pump:"+messageTemp;
-    mqtt_publish("esp32swim/notifications", (char*)notification.c_str());
+    else {
+      mqtt_pub_error("UNKNOWN Pump message triggered on "+topic_str, "\""+messageTemp+"\"");
+    }
+    mqtt_pub_notify("Pump "+messageTemp+" triggered on "+topic_str, "\""+messageTemp+"\"");
   }
+  
   // esp32swim/heater
-  else if (String(topic) == "esp32swim/heater") {
+  else if (mqtt_match_topic(topic_str, "heater")) {
     if(messageTemp == "on"){
-      Serial.println("on");
       digitalWrite(heater_RelayPin, HIGH);
     }
     else if(messageTemp == "off"){
-      Serial.println("off");
       digitalWrite(heater_RelayPin, LOW);
     }
-    notification = "heater:"+messageTemp;
-    mqtt_publish("esp32swim/notifications", (char*)notification.c_str());
+    else {
+      mqtt_pub_error("UNKNOWN Heater message triggered on "+topic_str, "\""+messageTemp+"\"");
+    }
+    mqtt_pub_notify("Heater "+messageTemp+" triggered on "+topic_str, "\""+messageTemp+"\"");
+  }
+
+  // esp32swim/restart
+  else if (mqtt_match_topic(topic_str, "restart")) {
+        mqtt_pub_notify("ESP Restart triggered");
+        ESP.restart();
   }
 }
