91d6041474efc2d80b46abd50675b2f81cb7c824
lightisright <x.correyeur@free.fr>
2022-05-23 13:57:46 +0200

    Created autonomous arrosage method

--- main/core_serre.ino
+++ main/core_serre.ino
@@ -1,9 +1,7 @@
-//essa função ficará mudando o estado do led a cada 1 segundo
-//e a cada piscada (ciclo acender e apagar) incrementará nossa variável blinked
 void core_serre( void * pvParameters ) {
   
     String taskMessage = "Core SERRE activated...";
     Serial.println(taskMessage);
 
-    hardcoded_arrosage();
+    arrosage_serre();
 }
--- main/serre.ino
+++ main/serre.ino
@@ -1,36 +1,71 @@
-#include <EEPROM.h>
-#define EEPROM_SIZE 1
+#include <Preferences.h>
 
-void hardcoded_arrosage() {
+int tempo_vanne = 30;   // tempo attente ouverture vanne (secondes)
 
-  EEPROM.begin(EEPROM_SIZE);
+void arrosage_serre() {
 
+  Preferences preferences;
+  int delay_minutes = 0;      // starting delay in minutes (also used internally between 2 arrosages)
+  int duration_minutes = 1;   // duration in minutes
+
+  // get delay & duration from memory
+  preferences.begin(esp32_id.c_str(), false);
+  unsigned int read_delay_minutes = preferences.getUInt("arrosage_serre_delay", delay_minutes);
+  unsigned int read_duration_minutes = preferences.getUInt("arrosage_serre_duration", duration_minutes);
+  preferences.end();
+
+  // configure GPIO pins
   pinMode(pump_RelayPin, OUTPUT);
   pinMode(heater_RelayPin, OUTPUT);
 
-  mqtt_pub_notify("Hardcoded arrosage activated.... waiting 10s....");
-
-  mqtt_pub_notify("hardcoded_arrosage: Electrovanne: ON....");
-  digitalWrite(heater_RelayPin, HIGH);
-
-  delay(20000); // attente ouverture vanne
-
-  mqtt_pub_notify("hardcoded_arrosage: Pompe: ON....");
-  digitalWrite(pump_RelayPin, HIGH);
-
-  mqtt_pub_notify("hardcoded_arrosage: Arrosage in progress 60s....");
-  delay(4*60000);
-
-  mqtt_pub_notify("hardcoded_arrosage: Pompe: OFF....");
-  digitalWrite(pump_RelayPin, LOW);
-
-  delay(50);
-
-  mqtt_pub_notify("hardcoded_arrosage: Electrovanne: OFF....");
-  digitalWrite(heater_RelayPin, LOW);
-
   while (true) {
-    mqtt_pub_notify("hardcoded_arrosage: Waiting 1h for ESP shutdown or restart.");
-    delay(3600000);
+
+    // wait configured duration
+    Serial.println("arrosage_serre: Waiting for arrosage start.");
+    int delaytime = read_delay_minutes*60 - (tempo_vanne+read_duration_minutes*60);  // retrait de la durée d'arrosage pour avoir toujours le bon délai d'arrosage
+    if ( delaytime < 0 ) {
+      delaytime=0;
+    }
+    Serial.print("Delay before next arrosage: ");
+    Serial.print(delaytime);
+    Serial.println(" secondes");
+    delay(delaytime*1000); 
+
+    _do_arrosage_serre(read_duration_minutes);
+
+    // Load new duration (if set) from memory
+    preferences.begin(esp32_id.c_str(), false);
+    unsigned int read_duration_minutes = preferences.getUInt("arrosage_serre_duration", duration_minutes);
+    preferences.end();
+    
+    // set next arrosage in 24 hours
+    //read_delay_minutes = 60*24;       // for real condition
+    read_delay_minutes = 60*1;       // for testing purpose
   }
+}
+
+void _do_arrosage_serre(unsigned int duration_minutes) {
+
+    // start arrosage
+    Serial.println("arrosage_serre: Arrosage starting....");
+  
+    Serial.println("arrosage_serre: Electrovanne: ON....");
+    digitalWrite(heater_RelayPin, HIGH);
+  
+    delay(tempo_vanne*1000); // attente ouverture vanne
+  
+    Serial.println("arrosage_serre: Pompe: ON....");
+    digitalWrite(pump_RelayPin, HIGH);
+  
+    Serial.println("arrosage_serre: Arrosage in progress....");
+    delay(duration_minutes*60*1000);
+  
+    Serial.println("arrosage_serre: Pompe: OFF....");
+    digitalWrite(pump_RelayPin, LOW);
+  
+    Serial.println("arrosage_serre: Electrovanne: OFF....");
+    digitalWrite(heater_RelayPin, LOW);
+
+    // start arrosage
+    Serial.println("arrosage_serre: Arrosage stopped.");
 }
